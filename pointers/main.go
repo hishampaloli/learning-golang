bbf12cb9ba7eb8395a68355bfc890b5547d0188f
hishampaloli <hishampaloli786@gmail.com>
2023-12-17 14:20:52 +0530

    pointer

---
+++ pointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcom to pointer")
+
+	// var ptr *int
+	// fmt.Println(ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+	fmt.Println(ptr)
+	fmt.Println(*ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println(myNumber)
+
+}
