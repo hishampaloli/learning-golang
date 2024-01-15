6708c1428aa14fbf482946cdd8892640a9219d70
hishampaloli <hishampaloli786@gmail.com>
2024-01-15 18:02:35 +0530

    struc

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-)
+) 
 
 const (
     conferenceName      = "Go Conference"
@@ -12,8 +12,15 @@ const (
 
 var (
     remainingTickets = uint(conferenceTickets)
-    bookings         []string
+	bookings = make([]UserData,0)
 )
+
+type UserData struct {
+	userName string
+	numberOfTickets uint
+}
+
+
 
 func greetUser(){
 	fmt.Printf("Welocme to our %v booking application \n", conferenceName)
@@ -21,9 +28,21 @@ func greetUser(){
 }
 
 
-func handleUpdateTickets(remainingTickets *uint, bookings *[]string, userTickets uint, userName string) {
+func PrintBookings(bookings []UserData){
+	firstNames := []string{}
+	for _, booking:= range bookings{
+		firstNames = append(firstNames, booking.userName)	
+	}
+	fmt.Printf("This are the attentees coming for the conferece := %v \n",firstNames)
+}
+
+func handleUpdateTickets(remainingTickets *uint, bookings *[]UserData, userTickets uint, userName string) {
 	*remainingTickets -= userTickets
-	*bookings = append(*bookings, userName)
+	var userData = UserData {
+		userName: userName,
+		numberOfTickets: userTickets,
+	}
+	*bookings = append(*bookings, userData)
 	
 	fmt.Printf("Thank you %v  for booking %v tickets, the details will be mailed to you \n", userName,userTickets)
 	fmt.Printf("%v are still remaining for you frients to join\n", *remainingTickets)
@@ -48,7 +67,8 @@ func main(){
 		}		
 
 		handleUpdateTickets(&remainingTickets,&bookings,uint(userTickets), userName + " " + lastName)
-		helper.PrintBookings(bookings)
+		fmt.Println(bookings)
+		PrintBookings(bookings)
 		if int(remainingTickets) <= 0 {
 			fmt.Println("Woof, you were the last to book, All slots are complted")
 			break
